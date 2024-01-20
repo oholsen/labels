d5f36e281b95dc044d79f3e5af939c27b4e78b88
Oystein Haug Olsen <oystein.olsen@omnysecurity.com>
2024-01-20 20:20:43 +0100

    clubwise for askersprinten on avry labels

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+from collections import defaultdict
 import pandas
+import sys
+import math
 
+# TODO: find labels that can tear/split
 # https://www.clasohlson.com/no/Selvheftende-etiketter/p/32-2618
+# 70x37mm
 
-dataframe = pandas.read_csv("records.csv")
-records = dataframe.to_dict(orient="records")
-
-
-# print(records)
+# print settings:
+# A4 with none margins and without headers and footers
 
 template_doc = open("template_doc.html").read()
 template_label = open("template_label.html").read()
-page_break = '<div class="page-break"></div>\n'
 
-current_klubb = None
 
 class Page:
     def __init__(self, name, page, labels = None) -> None:
@@ -34,74 +34,98 @@ class Page:
         return template_doc.replace("BODY", "".join(self.labels))
 
     def write(self):
-        if page.count() > 0:
-            with open(f"out/{self.name}-{self.page}.html", "w") as f:
+        if self.count() > 0:
+            filename = f"out/{self.name}-{self.page}.html"
+            print("Writing", filename)
+            with open(filename, "w") as f:
                 f.write(self.html())
 
     def next(self):
         return Page(self.name, self.page + 1)
 
 
-page = None
-klubber = {}
+def parse(filename: str):
+    dataframe = pandas.read_csv(filename)
+    records = dataframe.to_dict(orient="records")
+    for r in records:
+        # RaceSplitter start list, forventer følgende kolonner
+        # Racer bib number,First name,Last name,Team,Skive
+        # Add column Skive: =MOD(B1 - 1, 20) + 1
+        # Export as: CSV (UTF-8 encoded)
+        nummer = str(r["Racer bib number"])
+        fornavn = r["First name"]
+        etternavn = r["Last name"]
+        assert isinstance(fornavn, str)
+        assert isinstance(etternavn, str)
+        navn = fornavn + " " + etternavn
+        klubb = r["Team"]
+        skive = str(r["Skive"])
 
-for r in records:
-    #print(r)
-    nummer = str(r["Start nr"])
-    etternavn = str(r["Etternavn"])
-    fornavn = str(r["Fornavn"])
-    navn = fornavn + " " + etternavn
-    klubb = str(r["Klubb"])
-    skive = str(r["Skive"])
-    # if klubb == "nan": continue
+        #if len(klubb) > 18:
+        #    klubb = klubb[:18] + "..."
 
-    if klubb != current_klubb:
-        # labels.append(page_break)
-        klubber[klubb] = []
-        if page:
+        if not navn:
+            print("SKIP", r)
+            continue
+
+        # dataclass?
+        yield {
+            "NUMMER": nummer,
+            "KLUBB": klubb,
+            "NAVN": navn,
+            "SKIVE": skive,
+        }
+
+
+
+
+def clubwise(records):
+    klubber = defaultdict(list)
+    for r in records:
+        klubb = r["KLUBB"]
+        klubber[klubb].append(r)
+
+    if 0:
+        for klubb, records in klubber.items():
+            print(klubb, len(records))
+        return
+    for klubb, records in klubber.items():
+        n = len(records)
+        for r in records:
+            yield r
+        m = 3 - (n % 3)
+        if m == 3:
+            m = 0
+        # print(klubb, len(records), m)
+        assert (len(records) + m) % 3 == 0
+        for i in range(m):
+            yield {"KLUBB": klubb, "SKIVE": "", "NUMMER": "", "NAVN": ""}
+
+
+def main():
+
+    race = sys.argv[1]
+    filename = sys.argv[2]
+
+    page = Page(race, 1)
+
+    # TODO: kutte lange navn og klubbnavn
+    # TODO: shuffle clubs to avoid cuts and minimize sheets
+    # e.g. fill large clubs with small ones
+    # for o in parse(filename):
+    for o in clubwise(parse(filename)):
+            
+        if page.full():
             page.write()
-        page = Page(klubb, 1)
-        current_klubb = klubb
-        
-    if page.full():
-        # labels.append(page_break)
-        page.write()
-        page = page.next()
+            page = page.next()
 
-    # assert int(nummer) % 20 == int(skive), (nummer, skive)
-    #print(nummer, skive, fornavn, etternavn, klubb)
-    o = {
-        "NUMMER": nummer,
-        "KLUBB": klubb,
-        "NAVN": navn,
-        "SKIVE": skive,
-    }
-    html = template_label
-    for k, v in o.items():
-        html = html.replace(k, v)
-    # html = template_label.replace("NUMMER", nummer).replace("KLUBB", klubb).replace("NAVN", navn).replace("SKIVE", skive)
-    #print(html
-    page.add(html)
-    klubber[klubb].append(html)
+        html = template_label
+        for k, v in o.items():
+            html = html.replace(k, v)
+        page.add(html)
 
-    #break
+    page.write()
 
-#doc = template_doc.replace("BODY", "".join(labels))
-#print(doc)
 
-for k, v in klubber.items():
-    print(k, len(v))
-
-empty = ['<div class="label"></div>']
-empty_line = empty * 3
-
-def dump(klubb):
-    Page(klubb, 1, klubber[klubb]).write()
-    
-dump("Asker")
-dump("Bærum")
-dump("Try")
-fossum = klubber["Fossum"]
-Page("Fossum", 1, fossum[:24]).write()
-Page("Fossum", 2, fossum[24:48]).write()
-Page("Rest", 1, [fossum[48]] + empty*2 + empty_line + klubber["Gjerdrum"] + empty*2).write()
+if __name__ == "__main__":
+    main()
